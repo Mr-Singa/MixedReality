7561c0c81e6b14f915aa04562c97a2e1bbea2ffd
Poesbroek <yourischuring@gmail.com>
2021-10-08 22:24:55 +0200

    Mijn test troep

--- Keuzemodule_MR_VRTest/Assets/Youri/ChangeMatTexture.cs
+++ Keuzemodule_MR_VRTest/Assets/Youri/ChangeMatTexture.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToRenderTexture : MonoBehaviour
+public class ChangeMatTexture : MonoBehaviour
 {
-    public Camera targetCamera;
-    public RenderTexture cubeMapLeft;
-    
+    [SerializeField] private RenderTexture render;
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        targetCamera.RenderToCubemap(cubeMapLeft);
+               this.gameObject.GetComponent<Renderer>().material.SetTexture("_Tex", render); 
     }
 }
